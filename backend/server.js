5ceda15d0f0c7b16d53d9028901d4a94593aca23
delgado1999 <167220015+delgado1999@users.noreply.github.com>
2025-12-29 22:55:07 -0500

    correccion1

--- backend/server.js
+++ backend/server.js
@@ -3,39 +3,15 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-
-// ===============================
-// MIDDLEWARES
-// ===============================
 app.use(cors());
-app.use(express.json()); // Para recibir JSON en el body
+app.use(express.json()); 
 
-// ===============================
-// IMPORTAR RUTAS
-// ===============================
-
-// CIUDADES
 const ciudadRoutes = require('./routes/ciudad.routes');
-
-// CLIENTES 
 const clienteRoutes = require('./routes/cliente.routes'); 
-
-// VENDEDORES 
 const vendedorRoutes = require('./routes/vendedorRoutes');
-
-
-// BUSES 
 const busRoutes = require('./routes/busRoutes');
-
-// RESERVAS 
 const reservaRoutes = require('./routes/reservaRoutes');
-
-// DETALLE RESERVA 
 const detalleReservaRoutes = require('./routes/detalleReservaRoutes');
-
-// ===============================
-// USAR RUTAS (ENDPOINTS BASE)
-// ===============================
 
 app.use('/api/ciudades', ciudadRoutes);
 app.use('/api/clientes', clienteRoutes);
